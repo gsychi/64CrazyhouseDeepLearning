d78690941f1728a9365cd566780d82d4b279d7c0
Gordon <gordonchi11@gmail.com>
2018-10-14 16:37:59 -0400

    Update ChessConvNet.py

--- ChessConvNet.py
+++ ChessConvNet.py
@@ -12,26 +12,41 @@ class ChessConvNet(nn.Module):
         self.numClasses = num_classes
         super(ChessConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),  # 1, 32
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),  # 1, 16
             nn.BatchNorm2d(32),
-            nn.ReLU())
+            nn.ReLU()
+            )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2),  # 32, 32
-            nn.BatchNorm2d(32),  # 32
-            nn.ReLU())
+            nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2),  # 16, 16
+            nn.BatchNorm2d(32),  # 16
+            nn.ReLU()
+            )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
-            nn.BatchNorm2d(32),
-            nn.ReLU())
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 16, 16
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+            )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
-            nn.BatchNorm2d(32),
-            nn.ReLU())
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),  # 16, 16
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+            )
         self.layer5 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
-            nn.BatchNorm2d(32),
-            nn.ReLU())
-        self.fc = nn.Linear(896 * 32, num_classes)
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),  # 16, 16
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+            )
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),  # 16, 8
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+            )
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(64, 1, kernel_size=1, stride=1, padding=0),  # 8, 4
+            nn.BatchNorm2d(1),
+            nn.ReLU()
+            )
+        self.fc = nn.Linear(896 * 1, num_classes)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -39,6 +54,8 @@ class ChessConvNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
